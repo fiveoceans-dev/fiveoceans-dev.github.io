ec99b86554f8eeea97b756127f50efc7943b89a8
five <niip42@gmail.com>
2026-07-21 18:00:04 +0900

    c

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+const isGitHubPages = process.env.GITHUB_ACTIONS === 'true';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  base: isGitHubPages ? '/fiveoceans-edv.github.io/' : '/',
 });
